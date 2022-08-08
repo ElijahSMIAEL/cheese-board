aa95f107efca3c782e91deb2d852bdfe066319cc
Endrews <elijah.smiael@gmail.com>
2022-08-08 12:55:59 -0500

    add about and index view for cheeses

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('cheeses/', views.cheeses_index, name='cheeses'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,26 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+class Cheese:
+
+  def __init__(self, name, brand, age, description):
+    self.name = name
+    self.brand = brand
+    self.age = age
+    self.description = description
+
+cheeses = [
+  Cheese('Cheddar', 'Kraft', 0, 'A cheap ordinary cheddar cheese'),
+  Cheese('Pepper-Jack', 'Great Value', 0, 'A plain, yet delicious, pepper packed cheese'),
+  Cheese('Merlot BellaVitano', 'Sartori', 2, 'A rich, creamy cheese with berry and plum merlot notes')
+]
+
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Cheese</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def cheeses_index(request):
+  return render(request, 'cheeses/index.html', { 'cheeses' : cheeses })
